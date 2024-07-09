c9724772b8c4bf061e1ba644d704cf3dab017fbe
Jose <jpelayo53@gmail.com>
2024-07-08 17:22:27 -0700

    refactored from function constructors to ES6

--- script.js
+++ script.js
@@ -1,3 +1,186 @@
+// ES6 Classes
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    // method to toggle read
+    toggleRead() {
+        this.read = !this.read;
+    }
+}
+
+class Library {
+    constructor() {
+        this.myLibrary = [];
+        this.cardContainer = document.querySelector('.card-container');
+        this.openButton = document.querySelector('.open-button');
+        this.modal = document.querySelector('#modal');
+        this.titleInput = document.querySelector('#form-title');
+        this.authorInput = document.querySelector('#form-author');
+        this.pagesInput = document.querySelector('#form-pages');
+        this.readInput = document.querySelector('#form-read');
+        this.submitFormBtn = document.querySelector('#submit-form');
+
+        this.initialize();
+    }
+
+    // method to initialize
+    initialize() {
+        this.submitFormBtn.addEventListener('click', () => this.addBookFromForm());
+        this.openButton.addEventListener('click', () => this.modal.showModal());
+        this.loopThroughLibrary();
+    }
+
+    // method to add book from form
+    addBookFromForm() {
+        const title = this.titleInput.value;
+        const author = this.authorInput.value;
+        const pages = this.pagesInput.value;
+        const read = this.readInput.checked ? true : false;
+
+        const newBook = new Book(title, author, pages, read);
+        this.addBookToLibrary(newBook);
+        this.clearForm();
+        this.render();
+    }
+
+    // method to add book to library
+    addBookToLibrary(book) {
+        this.myLibrary.push(book);
+    }
+
+    // method to clear form
+    clearForm() {
+        this.titleInput.value = '';
+        this.authorInput.value = '';
+        this.pagesInput.value = '';
+        this.readInput.checked = false;
+    }
+
+    // method to loop through library
+    loopThroughLibrary() {
+        this.cardContainer.innerHTML = '';
+        this.myLibrary.forEach((book, index) => this.createBookCard(book, index));
+    }
+
+    // method  to create book card
+    createBookCard(book, index) {
+        const cardDiv = document.createElement('div');
+        cardDiv.className = 'card';
+        cardDiv.setAttribute('data-index', index);
+
+        const titleSpan = document.createElement('span');
+        titleSpan.textContent = `${book.title}`;
+
+        const authorSpan = document.createElement('span');
+        authorSpan.textContent = `By ${book.author}`;
+
+        const pagesSpan = document.createElement('span');
+        pagesSpan.className = 'pages';
+        pagesSpan.textContent = `Pages: ${book.pages}`;
+
+        const readBtn = this.createReadButton(book);
+        const removeBtn = this.createRemoveButton(index);
+
+        cardDiv.appendChild(titleSpan);
+        cardDiv.appendChild(authorSpan);
+        cardDiv.appendChild(pagesSpan);
+        cardDiv.appendChild(readBtn);
+        cardDiv.appendChild(removeBtn);
+
+        this.cardContainer.appendChild(cardDiv);
+    }
+
+    // method to create read button
+    createReadButton(book) {
+        const readBtn = document.createElement('button');
+        readBtn.className = 'cardReadBtn';
+        readBtn.textContent = book.read ? "Read" : "Not Read";
+        this.setReadButtonColor(readBtn, book.read);
+
+        readBtn.addEventListener('click', () => {
+            book.toggleRead();
+            readBtn.textContent = book.read ? "Read" : "Not Read";
+            this.setReadButtonColor(readBtn, book.read);
+        });
+
+        readBtn.addEventListener('mouseover', () => {
+            readBtn.style.backgroundColor = book.read ? "rgba(45, 106, 79, 0.7)" : "#e8edf4";
+        });
+
+        readBtn.addEventListener('mouseout', () => {
+            this.setReadButtonColor(readBtn, book.read);
+        });
+
+        return readBtn;
+    }
+
+    // method to set read button color
+    setReadButtonColor(button, read) {
+        button.style.backgroundColor = read ? "rgba(64, 145, 108, 0.7" : "#d1d5db";
+    }
+
+    // method to create button
+    createRemoveButton(index) {
+        const removeBtn = document.createElement('button');
+        removeBtn.className = 'cardRemoveBtn';
+        removeBtn.textContent = 'Delete';
+
+        removeBtn.addEventListener('click', () => {
+            this.myLibrary.splice(index, 1);
+            this.render();
+        });
+
+        return removeBtn;
+    }
+
+    // method to render
+    render() {
+        this.cardContainer.innerHTML = '';
+        this.loopThroughLibrary();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const library = new Library();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function Contructors 
+/*
+
 const myLibrary =  [];
 
 const cardContainer = document.querySelector('.card-container');
@@ -59,8 +242,6 @@ function Book(title, author, pages, read) {
        
         const cardDiv = document.createElement('div');
         cardDiv.className = 'card';
-
-        // Setting up data attribute to store index of the book
         cardDiv.setAttribute('data-index', index);
 
         const titleSpan = document.createElement('span');
@@ -114,14 +295,11 @@ function Book(title, author, pages, read) {
         removeBtn.className = 'cardRemoveBtn';
         removeBtn.textContent = "Delete";
 
-        // Add click event to the remove button
+        
         removeBtn.addEventListener('click', () => {
-            //Retrieve the index from the data attribute
             const dataIndex = cardDiv.getAttribute('data-index');
-            // Convert to number (index in myLibrary array)
             const indexToRemove = parseInt(dataIndex, 10);
 
-            //Remove the book from the array and update the DOM
             myLibrary.splice(indexToRemove, 1);
             cardContainer.innerHTML = '';
             loopThroughLibrary(myLibrary, cardContainer);
@@ -139,6 +317,7 @@ function Book(title, author, pages, read) {
 
     loopThroughLibrary(myLibrary, cardContainer);
 
+*/ 
 
 
     
